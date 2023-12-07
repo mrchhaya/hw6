b86e3758077edbf41a06354af9a1946860a00485
Mohit Chhaya <mohitchhaya24@gmail.com>
2023-12-06 23:25:46 -0600

    hw6

---
+++ q2/fun-untyped-kompiled/configVars.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+declaredConfigVar_IO='String'
+declaredConfigVar_STDIN='String'
+declaredConfigVar_IO='String'
+declaredConfigVar_PGM='Exp'
+declaredConfigVars=(
+    'IO'
+    'STDIN'
+    'IO'
+    'PGM'
+)
---
+++ q3/fun-untyped-kompiled/configVars.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+declaredConfigVar_PGM='Exp'
+declaredConfigVar_IO='String'
+declaredConfigVar_STDIN='String'
+declaredConfigVar_IO='String'
+declaredConfigVars=(
+    'PGM'
+    'IO'
+    'STDIN'
+    'IO'
+)
